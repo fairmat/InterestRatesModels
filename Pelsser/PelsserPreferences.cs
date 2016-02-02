55fe1ee7ba668c09de3d7f34981c3af3b634579e
matteot <matteo.tesser@gmail.com>
2016-02-02 08:27:42 +0100

    -added preferences class for Pelsser.

---
+++ Pelsser/PelsserPreferences.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DVPLI;
+using Mono.Addins;
+namespace Pelsser
+{
+
+    [SettingsContainer("Pelsser Squared Gaussian Model", SettingType = SettingType.Valuation)]
+    [Extension("/Fairmat/UserSettings")]
+    [Serializable]
+    public class PelsserPreferences: ISettings
+    {
+        [SettingDescription("Negative Rates Correction")]
+        public bool NegativeRatesCorrection = false;
+    }
+}
