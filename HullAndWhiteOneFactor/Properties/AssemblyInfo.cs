b0faf4f57b0f11344d56cd6fb168c7721c108a63
Enrico Favali <enrico.favali@fairmat.com>
2024-04-09 16:11:20 +0200

    [RD-FFK-NET8] Upgrated HullAndWhiteOneFactor project with double target netstandard2.0-net48, removed unused references fairmat/dev/fairmatserver#3493

--- HullAndWhiteOneFactor/Properties/AssemblyInfo.cs
+++ HullAndWhiteOneFactor/Properties/AssemblyInfo.cs
@@ -26,17 +26,6 @@ using Mono.Addins;
 [assembly: AddinAuthor("Fairmat SRL")]
 [assembly: AddinDescription("A no-arbitrage model which is the industry standard " +
                                "for modeling the future interest rate dynamic.")]
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Hull-WhiteOneFactorModel")]
-[assembly: AssemblyDescription("A no-arbitrage model which is the industry standard " +
-                               "for modeling the future interest rate dynamic.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("Hull-WhiteOneFactorModel")]
-[assembly: AssemblyCompany("Fairmat SRL")]
-[assembly: AssemblyCopyright("Copyright © Fairmat SRL 2009-2015")]
 [assembly: AssemblyTrademark("Fairmat")]
 [assembly: AssemblyCulture("")]
 
@@ -47,16 +36,3 @@ using Mono.Addins;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("82a890f6-c19a-42bb-a810-2d84c312e77e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.8")]
-[assembly: AssemblyFileVersion("1.1.8")]
